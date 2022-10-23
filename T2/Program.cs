a7619810592009862a2125378770010b3af03e71
Irina <lat-i@bk.ru>
2022-10-23 10:22:24 +0700

    Какое число больше из 2

---
+++ T2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите 2 числа: ");
+string numberAStr = Console.ReadLine();
+string numberBStr = Console.ReadLine();
+int numberA = int.Parse(numberAStr);
+int numberB = int.Parse(numberBStr);
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} больше {numberB}");
+}
+else
+{
+    Console.WriteLine($"Число {numberB} больше {numberA}");
+}
+
+
