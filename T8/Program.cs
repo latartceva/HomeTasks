5e52a83bbbe7efe83d04f9ab989157465415adbe
Irina <lat-i@bk.ru>
2022-10-23 11:44:03 +0700

    Все четные числа до Н

---
+++ T8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 2; i<=number; i++)
+
+if ( number % 2 == 0)
+{
+    Console.WriteLine($"{i}, ");
+}
