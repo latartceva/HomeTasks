ed74af6d8b61dc5c277e1b2533be0642c3dcbf35
Irina <lat-i@bk.ru>
2022-10-23 13:06:08 +0700

    Исправила ошибку

--- T8/Program.cs
+++ T8/Program.cs
@@ -6,8 +6,9 @@
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 for (int i = 2; i<=number; i++)
-
-if ( number % 2 == 0)
 {
+ if( i % 2 == 0)
+ {
     Console.WriteLine($"{i}, ");
+ }
 }
