b2e82de86d17ce7776cc75fb9bff3915a4ec7884
Irina <lat-i@bk.ru>
2022-11-06 19:42:56 +0700

    степень

---
+++ T25/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+
+Console.WriteLine("Ведите два числа  ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+double result = Math.Pow (numberA, numberB);
+
+ Console.WriteLine($"Степень числа {numberA} возведенное в {numberB} = {result}");
